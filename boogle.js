974277f26ebd35bcdb8767a88088b4330beed7f0
irsyahms <irsyahmardiahs@gmail.com>
2018-12-01 10:13:58 +0700

    add kamus and boggle.js

--- boogle.js
+++ boogle.js
@@ -1,1 +1,14 @@
+const kamus = require('./data.js');
 
+//value kamus bertipe object of array
+//silakan di-console.log sendiri
+
+class Boggle {
+  constructor() {
+
+  }
+}
+
+let boggle = new Boogle(4);
+boggle.shake();
+boggle.solve();
