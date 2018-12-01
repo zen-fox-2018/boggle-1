f46f0c88dd0a9a474d1760ac49de1b1f919f85fb
maitreya <bluespider529@gmail.com>
2018-12-01 15:01:45 +0700

    release 0 boggle

--- boogle.js
+++ boogle.js
@@ -4,11 +4,26 @@ const kamus = require('./data.js');
 //silakan di-console.log sendiri
 
 class Boggle {
-  constructor() {
+  constructor(number) {
+    this.board = [];
+    this.number = this.shake(number)
+  }
 
+  shake(size) {
+    
+    let alphabet = "abcdefghijklmopqrstuvwxyz";
+
+    for(let i = 0; i < size; i++) {
+      this.board.push([])
+      for(let j = 0; j < size; j++) {
+        let randomize = alphabet[Math.floor(Math.random()*alphabet.length)]
+        this.board[i].push(randomize);
+      }
+    }
+    return this.board
   }
 }
 
-let boggle = new Boogle(4);
-boggle.shake();
-boggle.solve();
+let boggle = new Boggle(4);
+console.log(boggle.shake());
+// boggle.solve();
