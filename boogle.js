13799d982bc6a57fa63c2c90f407bedc4211ea8c
ChristianHS23 <christian.sihotang23@gmail.com>
2018-12-02 11:31:27 +0700

    Hampir siap Masih ada yang error

--- boogle.js
+++ boogle.js
@@ -5,125 +5,31 @@ const kamus = require('./data.js');
 //console.log(kamus.words)
 class Boggle {
   constructor(size , kamus) {
-    this.size = size
-    this.kamus = kamus.words
-    // this.kamus= ['SAYA','SUKA','CODING','BANK','BCA','JAVASCRIPT','DAN','TIDUR']
-    // this.board= [ [ 'B', 'A', 'B', 'P', 'I', 'R', 'A', 'Q', 'X', 'W' ],
-    //               [ 'K', 'N', 'I', 'N', 'T', 'R', 'A', 'E', 'R', 'E' ],
-    //               [ 'K', 'R', 'U', 'N', 'T', 'W', 'K', 'H', 'W', 'I' ],
-    //               [ 'G', 'C', 'S', 'I', 'F', 'X', 'Y', 'P', 'C', 'O' ],
-    //               [ 'I', 'A', 'S', 'V', 'O', 'V', 'K', 'U', 'K', 'T' ],
-    //               [ 'G', 'Y', 'F', 'A', 'V', 'J', 'S', 'A', 'P', 'Z' ],
-    //               [ 'L', 'A', 'D', 'V', 'A', 'S', 'J', 'J', 'Y', 'E' ],
-    //               [ 'Z', 'E', 'A', 'A', 'Q', 'T', 'B', 'S', 'S', 'G' ],
-    //               [ 'H', 'U', 'O', 'Y', 'J', 'S', 'L', 'W', 'L', 'L' ],
-    //               [ 'D', 'G', 'Q', 'Z', 'U', 'N', 'M', 'O', 'R', 'H' ] ]
-    this.board = this.shake()
-    this.tempKamus = []
-    this.tempI = 0
-    this.tempJ = 0
-    this.tempStr = ''
-    this.findKamus = {}
-  }
-  checkKamus(stringDepan){ //ini cek depannya saja
-    for(let i = 0 ; i < this.kamus.length; i++){
-      if(stringDepan == this.kamus[i][0]){
-        this.tempKamus.push(this.kamus[i])
-        
-      }else if(this.tempKamus && this.kamus.length -1 == i){
-      //  console.log('Masuk')
-        return true
-      }
-      
-    }
+    // this.board = this.shake()
+    // this.size = size
+    // this.kamus = kamus.words
+    this.size = 4
+    this.kamus= ['SAYA','SUKA','CODING','BANK','BCA','JAVASCRIPT','DAN','TIDUR','RUN','SUSU']
+    this.board= [ [ 'I', 'A', 'B', 'S' ],
+                  [ 'D', 'N', 'G', 'S' ],
+                  [ 'O', 'K', 'U', 'N' ],
+                  [ 'A', 'C', 'S', 'I' ] ]
+   
     
-    return false
   }
-  chekKata(countNow , string){
-    
-    for(let i = 0; i < this.tempKamus.length; i++){
-       
-        if(this.tempKamus[i][countNow] == string){
-          return true
-        
+
+
+  solve(){
+    let hasil = []
+    for(let i = 0; i < this.kamus.length; i++){
+      if(this.cekKamus(this.kamus[i]).length){
+        hasil.push(this.kamus[i])
+        //console.log(hasil,'ini hasil')
       }
     }
-    return false
+    return hasil
   }
- checkAllKata(){
-   for(let i = 0; i < this.tempKamus.length; i++) {
 
-       if(this.tempKamus[i].slice(0,this.tempStr.length) == this.tempStr) {
-         return true
-       }
-     
-   }
-   return false
- }
-
-  checkDalam(coorI,coorJ,k){
-        
-          if(coorI - 1 >= 0 && this.chekKata(k , this.board[coorI - 1][coorJ])  && this.checkAllKata()){
-            this.tempI = coorI - 1
-            this.tempJ = coorJ
-            this.tempStr += this.board[coorI - 1][coorJ]
-            return true
-          } else if(coorI - 1 >= 0 && coorJ + 1 < this.size && this.chekKata(k , this.board[coorI - 1][coorJ + 1]) && this.checkAllKata() ){
-            this.tempI = coorI - 1
-            this.tempJ = coorJ + 1
-            this.tempStr +=  this.board[coorI - 1][coorJ + 1]
-            return true
-          }  else if(coorJ + 1 < this.size  && this.chekKata(k , this.board[coorI][coorJ + 1]) && this.checkAllKata() )  {
-            this.tempI = coorI 
-            this.tempJ = coorJ + 1
-            this.tempStr +=  this.board[coorI][coorJ + 1]
-            return true
-          } else if ( coorI + 1 < this.size && coorJ + 1 < this.size && this.chekKata(k, this.board[coorI + 1][coorJ + 1])  && this.checkAllKata() ) {
-            this.tempI = coorI + 1
-            this.tempJ = coorJ + 1
-            this.tempStr +=  this.board[coorI + 1][coorJ + 1]
-            return true
-          } else if( coorI + 1 < this.size  && this.chekKata(k , this.board [coorI + 1][coorJ]) && this.checkAllKata() ){
-            this.tempI = coorI + 1
-            this.tempJ = coorJ 
-            this.tempStr +=  this.board[coorI + 1][coorJ]
-            return true
-          } else if(coorJ - 1 >= 0 && coorI + 1 < this.size && this.chekKata(k, this.board[coorI + 1][coorJ - 1]) && this.checkAllKata() ){
-            this.tempI = coorI + 1
-            this.tempJ = coorJ - 1
-            this.tempStr +=  this.board[coorI + 1][coorJ - 1]
-            return true
-          } else if(coorJ - 1 >= 0 && this.chekKata(k , this.board [coorI][coorJ - 1]) && this.checkAllKata() ){
-            this.tempI = coorI
-            this.tempJ = coorJ - 1
-            this.tempStr +=  this.board[coorI][coorJ - 1]
-            return true
-          } else if(coorI - 1 >= 0 && coorJ - 1 >= 0 && this.chekKata(k, this.board[coorI - 1][coorJ - 1])  && this.checkAllKata() ){
-            this.tempI = coorI - 1
-            this.tempJ = coorJ - 1
-            this.tempStr +=  this.board[coorI - 1][coorJ - 1]
-            return true
-          }  
-          // else if( coorI + 1 < this.size && coorJ - 1 >= 0 && this.chekKata(k , this.board [coorI + 1][coorJ - 1]) ){
-          //   this.tempI = coorI + 1
-          //   this.tempJ = coorJ - 1 
-          //   this.tempStr +=  this.board[coorI + 1][coorJ - 1]
-          //   return true
-          // }
-          else {
-            this.tempI = 0
-            this.tempJ = 0
-            return false
-          }
-        
-  }
-  checkFound(){
-    for(let i = 0; i < this.tempKamus.length; i++){
-      if(this.tempStr == this.tempKamus[i]){
-        return true
-      }
-    }
-  }
   sleep (milliseconds) {
     var start = new Date().getTime();
     for (var i = 0; i < 1e7; i++) {
@@ -133,56 +39,86 @@ class Boggle {
     }
   }
   
+  cekKamus(kataKamus){
+    let temp = []
+    let k = 0
+    let dummyBoard = this.board.map(function(x){
+      return x.slice(0)
+    })
+    let cekKataPertama = this.kataPertama(kataKamus[0])
+    if(cekKataPertama.length){
 
-  solve(){
-    
-    for(let i = 0 ; i < this.board.length; i++){
-      this.tempStr=''
-      let k = 1
-      this.tempKamus = []
-      this.tempI=0
-      this.tempJ=0
-      for(let j = 0; j < this.board[i].length; j++){
-        this.tempI = i
-          this.tempJ = j
-        if( this.checkKamus(this.board[i][j]) ) {
-          
-          this.tempStr += this.board[i][j]
-         // console.log(`Masuk di IF 1~~~~Ini I dan j ${this.tempI} ${this.tempJ} ini temp str ${this.tempStr}`)
-          while(true){
-            //  console.log(`Ini I dan j ${this.tempI} ${this.tempJ} ini temp str ${this.tempStr} 
-            // Dalam While
-            // // Cek K di posisi ${k}
-            // // ini Current hasil ${Object.getOwnPropertyNames(this.findKamus)}`)
-            // // this.sleep(100)
-            if(this.checkDalam(this.tempI,this.tempJ,k)){
-              if(this.checkFound()){
-              //  console.log('~~masuk')
-                if(!this.findKamus[this.tempStr]){
-                  this.findKamus[this.tempStr] = 0
-                }
-                this.tempI = 0
-                this.tempJ = 0
-                this.tempStr = ''
-                this.tempKamus = []
-                break
-              }
-              k++
-            } else{
-                this.tempI = 0
-                this.tempJ = 0
-                this.tempStr = ''
-                this.tempKamus = []
-                k=1
-                break
-            }
+      temp.push(cekKataPertama)
+    } else {
+      return []
+    }
+    while(kataKamus[k+1] != undefined){ //Mencari kata selanjutnya ada di papan atau tidak
+     
+      for(let i = 0; i < temp[k].length; i ++){
+        
+        let coorITemp = temp[k][i].coorI
+        let coorJTemp = temp[k][i].coorJ
+        dummyBoard[coorITemp][coorJTemp] = ' '   
+         console.log(temp,'Ini temp')
+         console.log(dummyBoard)
+        let check = this.cekDalam([coorITemp,coorJTemp],kataKamus[k+1], dummyBoard)
+        if(check.length){
+          temp.push(check)
+        }
+         else if(i -1 < 0){
+          return []
+        }
+      }
+      k++
+    }
+    return temp
+  }
 
-          }
+  cekDalam(coorIndex , string , papan){
+    let hasil = []
+    let coorAwal = [coorIndex[0] -1 ,coorIndex[1] - 1]
+    let coorAkhir = [coorIndex[0] + 1, coorIndex[1] +1]
+    if(coorAwal[0] < 0){
+      coorAwal[0] = 0
+    }
+    if(coorAwal[1] < 0){
+      coorAwal[1] = 0
+    }
+    if(coorAkhir[0] >= this.size ){
+      coorAkhir[0] = this.size - 1
+    }
+    if(coorAkhir[1] >= this.size  ){
+      coorAkhir[1] = this.size - 1
+    }
+    for(let i = coorAwal[0]; i <= coorAkhir[0]; i++){
+      for(let j = coorAwal[1]; j <= coorAkhir[1]; j++){
+        if(papan[i][j] == string){
+          hasil.push({
+            coorI: i,
+            coorJ: j
+          })
         }
       }
     }
-    return Object.getOwnPropertyNames(this.findKamus)
+    // console.log(hasil,'Ini Coor hasil yang masuk ~~~~~')
+    return hasil
   }
+
+  kataPertama(string){
+    let arrayFirst=[]
+    for(let i = 0; i < this.board.length; i++){
+      for(let j = 0; j < this.board[i].length; j++){
+        if(this.board[i][j] === string){
+          arrayFirst.push({
+            coorI : i,
+            coorJ : j
+          })
+        }
+      }
+    }
+    return arrayFirst
+  }
+
 
   shake () {
     let  output = []
@@ -201,10 +137,11 @@ class Boggle {
 
 }
 
-let boggle = new Boggle(10 , kamus);
+let boggle = new Boggle(8 , kamus);
 //console.log(boggle.shake());
 console.log(boggle.board)
 console.log(boggle.solve());
-// string = 'sdssdsa'
-// console.log(string.slice(0,2))
+
+
+console.log(boggle.kataPertama('S'))
 
